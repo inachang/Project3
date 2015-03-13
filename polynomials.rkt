c29aaca052441a63212f01a56007859e777e7156
inachang <ina@Inas-Mac.local>
2015-03-13 10:51:45 -0700

    Formatted

--- polynomials.rkt
+++ polynomials.rkt
@@ -4,10 +4,13 @@
 ;COMP333 Project 3 - Polynomials
 
 (define coeff (lambda (t)
-                (car t)))
+                (car t)
+                )
+  )
 
 (define expon (lambda (t)
-                (last t)))
+                (last t))
+  )
 
 (define printTerm (lambda (t)
                     (if (= (coeff t) 0) (display "0") 
@@ -24,7 +27,12 @@
                               ((= e 1) (display "x"))
                               (else
                                (display "x^")
-                               (display e))))))))
+                               (display e)))
+                            )
+                          )
+                        )
+                    )
+  )
 
 (define printpoly (lambda (p)
                     (cond
@@ -41,74 +49,83 @@
                                    (begin
                                      (printTerm (car p))
                                      (display " + ")
-                                     (printpoly (cdr p)))))))))))
+                                     (printpoly (cdr p))))
+                               )
+                             )
+                         )
+                       )
+                      )
+                    )
+  )
 
 (define evalpoly (lambda (p v)
                    (if (= (length p) 1)
                       (* (coeff (car p)) (expt v (expon (car p))))
                       (+ (* (coeff (car p)) (expt v (expon (car p))))
-                        (evalpoly (cdr p) v)))))
+                        (evalpoly (cdr p) v)))
+                   )
+  )
                    
 (define GT (lambda (t1 t2)
-             (if (> (expon t1) (expon t2)) #t #f)))
+             (if (> (expon t1) (expon t2)) #t #f))
+  )
 
 (define EQExp? (lambda (t1 t2)
-                 (if (= (expon t1) (expon t2)) #t #f)))
+                 (if (= (expon t1) (expon t2)) #t #f))
+  )
 
 
 
 (define addTerm (lambda (t1 t2)
-                  (list (+ (coeff t1) (coeff t2)) (expon t1)))) 
+                  (list (+ (coeff t1) (coeff t2)) (expon t1)))
+  ) 
 
 
 (define simplify (lambda (p)
-  (simplifyRec (sort p GT))
-))
+                   (simplifyRec (sort p GT)))
+  )
 
 (define simplifyRec (lambda (p)
-  (if (<= (length p) 1) (list (car p))
-     (let*(
-       [t1 (car p)]
-       [t2 (cadr p)]
-      )
-      (if (EQExp? t1 t2 )
-       (if (> (length(cddr p)) 0)
-        (simplifyRec (append (list (addTerm t1 t2)) (cddr p)))
-        (append (list (addTerm t1 t2)))
-       )
-       (append (list (car p)) (simplifyRec (cdr p)))
-      )
-     ) 
+                      (if (<= (length p) 1) (list (car p))
+                          (let*(
+                                [t1 (car p)]
+                                [t2 (cadr p)]
+                                )
+                            (if (EQExp? t1 t2 )
+                                (if (> (length(cddr p)) 0)
+                                    (simplifyRec (append (list (addTerm t1 t2)) (cddr p)))
+                                    (append (list (addTerm t1 t2))))
+                                (append (list (car p)) (simplifyRec (cdr p))))) 
+                          )
+                      )
   )
-))
 
 (define addpoly (lambda (p1 p2)
- (simplify (append p1 p2))
-))
+                  (simplify (append p1 p2)))
+  )
 
 
 (define subtractpoly (lambda (p1 p2)
- (simplify (append p1 (map (lambda (x) (list (* (car x) -1) (cadr x))) p2)))
-))
+                       (simplify (append p1 (map (lambda (x) (list (* (car x) -1) (cadr x))) p2))))
+  )
 
 (define multiplyterms (lambda (t1 t2)
-  (list (* (coeff t1)(coeff t2)) (+ (expon t1)(expon t2)))
-))
+                        (list (* (coeff t1)(coeff t2)) (+ (expon t1)(expon t2))))
+  )
 
 (define multiplytermpoly (lambda (t1 p1)
-  (simplify (map (lambda (x) (multiplyterms t1 x)) p1))
-))
+                           (simplify (map (lambda (x) (multiplyterms t1 x)) p1)))
+  )
 
 (define multiplypolyRec (lambda (p1 p2 acc)
-  (if  (= (length p1) 0)
-    acc
-    (multiplypolyRec (cdr p1) p2 (append acc (multiplytermpoly (car p1) p2)))
+                          (if  (= (length p1) 0)
+                               acc
+                               (multiplypolyRec (cdr p1) p2 (append acc (multiplytermpoly (car p1) p2))))
+                          )
   )
-))
 
 (define multiplypoly (lambda (p1 p2)
-  (let* ([acc '()])
-   (simplify (multiplypolyRec p1 p2 acc))
+                       (let* ([acc '()])
+                         (simplify (multiplypolyRec p1 p2 acc)))
+                       )
   )
-  
-))
